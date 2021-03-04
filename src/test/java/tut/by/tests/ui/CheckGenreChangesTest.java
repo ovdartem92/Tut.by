985bed8f44b4bb7a107757260d422714a428abec
Artsem Audziyenka <Artsem_Audziyenka@epam.com>
2021-03-04 08:42:50 +0300

    ref: add minor changes

--- src/test/java/tut/by/tests/ui/CheckGenreChangesTest.java
+++ src/test/java/tut/by/tests/ui/CheckGenreChangesTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 import java.util.List;
 
 public class CheckGenreChangesTest extends BaseConfigurationTest {
-    HomePage homePage = new HomePage();
+    private HomePage homePage = new HomePage();
 
     @Test(description = "Check the ability to search for films by genre in the online cinema")
     public void scenarioOne() {
@@ -28,14 +28,14 @@ public class CheckGenreChangesTest extends BaseConfigurationTest {
     @Test(description = "Check the ability to search for serials by genre in the online cinema")
     public void scenarioTwo() {
         String genre = "Ужасы";
-        List<WebElement> moveResult = homePage
+        List<WebElement> serialResult = homePage
                 .open()
                 .clickAfishaButton()
                 .clickOnlineCinemasButton()
                 .clickSerialsButton()
                 .selectSerialsGenre(genre)
                 .getAllResultsOnPage();
-        for (WebElement webElement : moveResult) {
+        for (WebElement webElement : serialResult) {
             Assert.assertTrue(webElement.getText().contains(genre),
                     "The serial doesn't contains the desired genre: " + genre);
         }
@@ -44,14 +44,14 @@ public class CheckGenreChangesTest extends BaseConfigurationTest {
     @Test(description = "Check the ability to search for animations by genre in the online cinema")
     public void scenarioThree() {
         String genre = "Мелодрама";
-        List<WebElement> moveResult = homePage
+        List<WebElement> animationResult = homePage
                 .open()
                 .clickAfishaButton()
                 .clickOnlineCinemasButton()
                 .clickAnimationButton()
                 .selectAnimationsGenre(genre)
                 .getAllResultsOnPage();
-        for (WebElement webElement : moveResult) {
+        for (WebElement webElement : animationResult) {
             Assert.assertTrue(webElement.getText().contains(genre),
                     "The animations doesn't contains the desired genre: " + genre);
         }
